addfe6512b1e7a42253999286c7d00fd05a7e485
João Victor Rocha Vilela Godoi <80182535+Joao-Victor-RVG@users.noreply.github.com>
2023-02-20 16:47:42 -0300

    Lista 01

--- Exercises/Lista 001/ex 026/src/Main.java
+++ Exercises/Lista 001/ex 026/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int data,dia,mes,ano,ndata;
+
+        System.out.println("Digite data no formato dd/mm/aa:");
+        Scanner sc = new Scanner(System.in);
+        data = sc.nextInt();
+
+        dia = data / 10000;
+
+        mes = data % 10000 / 100;
+
+        ano = data % 100;
+
+        ndata = mes * 10000 + dia  * 100 + ano;
+
+        System.out.println("Dia " + dia);
+        System.out.println("Mes " + mes);
+        System.out.println("Ano " + ano);
+        System.out.println("DATA NO FORMATO MMDDAA: " + ndata);
+
+
     }
 }
